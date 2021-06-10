e280148d5e1f2bf5ab85f12620b36469c87eff20
andrescruz5193@gmail.com <andrescruz5193@gmail.com>
2021-06-10 16:32:54 -0500

    Primer commit proyecto mercado-liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { dirname } = require('path');
+const app = express();
+const path = require('path');
+
+
+app.use( express.static( path.resolve(__dirname, './public') ) );
+
+app.listen( 3005, ()=>{
+    console.log('Servidor escuchando por el puerto 3005');
+} );
+
+app.get( '/',  ( req, res )=>{
+    res.sendFile( path.resolve(__dirname, './views/home.html') );
+});
+
+app.get( '/register', ( req, res )=>{
+    res.sendFile( path.resolve(__dirname, './views/register.html') )
+});
+
+app.get( '/login', ( req, res )=>{
+    res.sendFile( path.resolve(__dirname, './views/login.html') )
+});
